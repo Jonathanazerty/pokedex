3f4653efe253d9af3aa626831199b723cc72c49f
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-06 11:15:59 +0200

    Add correct info inside of routing buttons

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Home from './Home';
 function App() {
   return (
     <Router>
-        <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> To Do </button></Link>
-        <Link to="/pokedex"><button className="btn btn-outline-light col-12 p-2" type="button"> Done</button></Link>
+        <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> Homepage </button></Link>
+        <Link to="/pokedex"><button className="btn btn-outline-light col-12 p-2" type="button"> Pokedex</button></Link>
       <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/pokedex" component={Pokedex}/>
