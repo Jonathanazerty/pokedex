257741adeedf28075bcd4053d7cd0cae1d751847
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-06 11:13:02 +0200

    Link react-router-DOM, WIP fix Bootstrap before pushing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import React from 'react';
+import Pokedex from './Pokedex';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
         <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> To Do </button></Link>
-        <Link to="/test"><button className="btn btn-outline-light col-12 p-2" type="button"> Done</button></Link>
+        <Link to="/pokedex"><button className="btn btn-outline-light col-12 p-2" type="button"> Done</button></Link>
       <Switch>
         <Route path="/" exact component={Home}/>
-        <Route path="/test" component={Test}/>
+        <Route path="/pokedex" component={Pokedex}/>
       </Switch>
     </Router>
   )
---
+++ src/Pokedex.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Pokedex() {
+    return (
+        <div>
+            <p>This is the Pokedex page</p>
+        </div>
+    )
+}
