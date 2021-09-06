e23117eefe762b44f742ef755b765b48f0732d35
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-06 16:13:35 +0200

    Prepare HTML structure for Pokedex page ( WIP )

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import React from 'react';
 import Pokedex from './Pokedex';
 import Home from './Home';
 
 const App = () => {
+  
   return (
     <Router>
         <Link to="/"><button className="btn btn-outline-light col-12 p-2" type="button"> Homepage </button></Link>
         <Link to="/pokedex"><button className="btn btn-outline-light col-12 p-2" type="button"> Pokedex</button></Link>
       <Switch>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact component={Home} />
         <Route path="/pokedex" component={Pokedex}/>
       </Switch>
     </Router>
+
   )
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import './dexStyling.css'
 
 
 export default function Home() {
     return (
         <>
-            <label htmlFor="pokemonpic" id="pokemonpic"></label>
-            <label htmlFor="addToDo" id="what2do"> Type in a number and find out which Pokémon you will get ! </label>
-            <input /*ref={inputRef}*/ type="text" name="search" id="search" placeholder="Find your Pokémon here !" />
-            <input /*onClick={clickHandler}*/ type="submit" value="catch" id="submit"/>
-
-            <label htmlFor="pokedetails" id="pokedetails"></label>
+        <p>This is the Home page</p>
         </>
-        );
+    )
 
 }
