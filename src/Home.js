eb9b3d19d51608fefbe4bcf7c8a61913ce4a4768
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-09-06 11:21:53 +0200

    adding homepage with label and input WIP

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+export default function PokemonList() {
+
+    return (
+        <>
+            <label htmlFor="pokemonpic" id="pokemonpic"></label>
+            <label htmlFor="addToDo" id="what2do"> Type in a number and find out which Pokémon you will get ! </label>
+            <input /*ref={inputRef}*/ type="text" name="search" id="search" placeholder="Find your Pokémon here !" />
+            <input /*onClick={clickHandler}*/ type="submit" value="catch" id="submit"/>
+
+            <label htmlFor="pokedetails" id="pokedetails"></label>
+        </>
+        );
+
+}
