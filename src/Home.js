df566a484d4ce4cbc1ab67223253025a04355ef1
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-08 10:36:44 +0200

    Jsais pas maar mergen

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './dexStyling.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import axios from 'axios';
+import axios from 'axios';
 
 
 export default function Home( props ) {
@@ -37,18 +37,13 @@ export default function Home( props ) {
     //--------------------------------------------------------------------------------
 
     const [pokemon, setPokemon] = useState([]);
-    const urlPokemon = 'https://pokeapi.co/api/v2/pokemon?limit=100';
 
     useEffect((pokemon) => {
-        async function fetchData() {
-            let response = await fetch (urlPokemon);
-            let pokemonData = await response.json();
-            console.log(pokemonData.results);
-            console.log(pokemonData.results[0].name);
-            console.log(pokemonData.results[0].url);
-        }
-        fetchData();
-    }, []);
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=100").then(res => {
+        setPokemon(res.data.results.map(p => p.name))
+      })
+    }, [])
+    console.log(pokemon);
 
     return (
         <div>
