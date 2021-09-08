f1fe7b59833114bebe3f2317dd449f3c985dda33
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-09-08 10:34:18 +0200

    make pokemon appear on homepage

--- src/Home.js
+++ src/Home.js
@@ -2,59 +2,46 @@ import React, {useState, useEffect} from 'react';
 import './dexStyling.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-// import axios from 'axios';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 
-export default function Home( props ) {
-    // busy with Basile ----------------------------------------------------------
-    // export default function Home( props ) {
-    //     const [pokemon, setPokemon] = useState([
-    //         {'name':""}
-    //     ])
-    //     const [pokeData, setPokeData] = useState([])
-      
-    //     useEffect(() => {
+
+const Home = () => {
     
-    //         axios.get("https://pokeapi.co/api/v2/pokemon?limit=100").then(res => {
-    //         res.data.results.map((detailsPokemon) => {
-    //             setPokemon(...pokemon, {'name':detailsPokemon.name});
-    //             console.log(detailsPokemon.url);
-    //         })
-    //       })
-    //     }, [])
-    //     console.log(pokemon);
-  
-    // const [pokemon, setPokemon] = useState([]);
-    // useEffect(() => {
-    // ----------------------------------------------------------------------------
-  
-    // Works ----------------------------------------------------------------------
-    //   axios.get("https://pokeapi.co/api/v2/pokemon?limit=100").then(res => {
-    //     setPokemon(res.data.results.map(p => p.name))
-    //   })
-    // }, [])
-    // console.log(pokemon);
-    //--------------------------------------------------------------------------------
-
     const [pokemon, setPokemon] = useState([]);
-    const urlPokemon = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+    useEffect(()=> {
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+            .then(function (res) {
+                setPokemon(res.data.results)
+                // console.log(res.data.results)
+            })
+            .catch(function (err) {
+                // handle error
+                console.log(err);
+            })
 
-    useEffect((pokemon) => {
-        async function fetchData() {
-            let response = await fetch (urlPokemon);
-            let pokemonData = await response.json();
-            console.log(pokemonData.results);
-            console.log(pokemonData.results[0].name);
-            console.log(pokemonData.results[0].url);
-        }
-        fetchData();
-    }, []);
+    },[]);
+
+    
+
+    // const [pokemon, setPokemon] = useState([]);
+    // const urlPokemon = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+
+    // useEffect((pokemon) => {
+    //     async function fetchData() {
+    //         let response = await fetch (urlPokemon);
+    //         let pokemonData = await response.json();
+    //         console.log(pokemonData.results);
+    //     }
+    //     fetchData();
+    // }, []);
 
     return (
         <div>
-            {/* {pokemon.map(p => (
-                <div key={p}>{p}</div>
-            ))} */}
+            {pokemon.map((item, index) => {
+                    return <li key={index}><Link to={"pokemon?id="+index+"&name="+item.name} details={item.url} >{item.name}</Link></li>
+                })}
         <>
         <Header />
         <Footer />
@@ -62,3 +49,5 @@ export default function Home( props ) {
         </div>
     )
 }
+
+export {Home};
